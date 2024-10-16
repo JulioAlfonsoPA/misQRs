6a27e08e410abe6000467d08615dfd6e3da322a1
JulioAlfonsoPA <killerbenito0007@gmail.com>
2024-10-16 17:54:52 -0600

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxNRfWb8-MWFBBm6MuW-uITLgAQ9nkeoM0WI2weJoC35wEG5eaFAhi2KUS2dh_lGTESbw/exec";
